204f02c96802ad2b824c86856859939c885d4267
ha <alirezamosavi346@gmail.com>
2021-02-15 02:50:24 +0330

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,296 @@
+<template>
+  <div>
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4">{{ product.title }}</p>
+        </div>
+        <div>
+          <button class="button is-small" :title="removeFromFavouriteLabel" v-show="product.isFavourite" @click="removeFromFavourite(product.id)">
+            <span class="icon is-small">
+              <i class="fa fa-heart"></i>
+            </span>
+          </button>
+          <button class="button is-small" :title="addToFavouriteLabel" v-show="!product.isFavourite" @click="saveToFavorite(product.id)">
+            <span class="icon is-small">
+              <i class="fa fa-heart-o"></i>
+            </span>
+          </button>
+        </div>
+      </div>
+      <div class="content is-clearfix">
+        <p>{{ product.description }}</p>
+        <div class="is-pulled-left">
+          <i v-if="product.ratings === 1" class="fa fa-star"></i>
+          <i v-if="product.ratings === 2" class="fa fa-star"></i>
+          <i v-if="product.ratings === 2" class="fa fa-star"></i>
+          <i v-if="product.ratings === 3" class="fa fa-star"></i>
+          <i v-if="product.ratings === 3" class="fa fa-star"></i>
+          <i v-if="product.ratings === 3" class="fa fa-star"></i>
+          <i v-if="product.ratings === 4" class="fa fa-star"></i>
+          <i v-if="product.ratings === 4" class="fa fa-star"></i>
+          <i v-if="product.ratings === 4" class="fa fa-star"></i>
+          <i v-if="product.ratings === 4" class="fa fa-star"></i>
+          <i v-if="product.ratings === 5" class="fa fa-star"></i>
+          <i v-if="product.ratings === 5" class="fa fa-star"></i>
+          <i v-if="product.ratings === 5" class="fa fa-star"></i>
+          <i v-if="product.ratings === 5" class="fa fa-star"></i>
+          <i v-if="product.ratings === 5" class="fa fa-star"></i>
+          <p>{{ product.reviews > 0 ? `${product.reviews} Reviews` : 'No reviews' }}</p>
+        </div>
+        <p class="is-pulled-right">
+          <span class="title is-4"><strong>&euro; {{ product.price }}</strong></span>
+        </p>
+      </div>
+      <div class="card-footer btn-actions">
+        <div class="card-footer-item field is-grouped">
+          <div class="buttons">
+            <button class="button is-primary" v-if="!product.isAddedToCart" @click="addToCart(product.id)">{{ addToCartLabel }}</button>
+            <button class="button is-text" v-if="product.isAddedToCart" @click="removeFromCart(product.id, false)">{{ removeFromCartLabel }}</button>
+          </div>
+           <div class="select is-rounded is-small">
+            <!-- <select @change="onSelectQuantity(product.id)" v-model="selected">
+              <option v-for="quantity in quantityArray" :value="quantity">{{ quantity }}</option>
+            </select> -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <nuxt-link
+      class="details"
+      :to="{
+        name: 'product_detail-id',
+        params: {
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          rating: product.ratings,
+          reviews: product.reviews,
+          isAddedBtn: product.isAddedBtn
+        }
+      }"
+    >
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'products',
+  props: ['product'],
+
+  data () {
+    return {
+      addToCartLabel: 'Add to cart',
+      viewDetailsLabel: 'Details',
+      removeFromCartLabel: 'Remove from cart',
+      addToFavouriteLabel: 'Add to favourite',
+      removeFromFavouriteLabel: 'Remove from favourite',
+      selected: 1,
+      quantityArray: [],
+      products: [
+    {
+      id: 1,
+      title: 'Product 1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      price: 50,
+      ratings: 3,
+      reviews: 5,
+      isAddedToCart: false,
+      isAddedBtn: false,
+      isFavourite: false,
+      quantity: 1
+    },
+    {
+      id: 2,
+      title: 'Product 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      price: 35,
+      ratings: 5,
+      reviews: 10,
+      isAddedToCart: false,
+      isAddedBtn: false,
+      isFavourite: false,
+      quantity: 1
+    },
+    {
+      id: 3,
+      title: 'Product 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      price: 110,
+      ratings: 2,
+      reviews: 3,
+      isAddedToCart: false,
+      isAddedBtn: false,
+      isFavourite: false,
+      quantity: 1
+    },
+    {
+      id: 4,
+      title: 'Product 4',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      price: 50,
+      ratings: 1,
+      reviews: 0,
+      isAddedToCart: false,
+      isAddedBtn: false,
+      isFavourite: false,
+      quantity: 1
+    },
+    {
+      id: 5,
+      title: 'Product 5',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      price: 35,
+      ratings: 4,
+      reviews: 2,
+      isAddedToCart: false,
+      isAddedBtn: false,
+      isFavourite: false,
+      quantity: 1
+    },
+    {
+      id: 6,
+      title: 'Product 6',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      price: 110,
+      ratings: 5,
+      reviews: 1,
+      isAddedToCart: false,
+      isAddedBtn: false,
+      isFavourite: false,
+      quantity: 1
+    },
+    {
+      id: 7,
+      title: 'Product 7',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      price: 50,
+      ratings: 5,
+      reviews: 7,
+      isAddedToCart: false,
+      isAddedBtn: false,
+      isFavourite: false,
+      quantity: 1
+    },
+    {
+      id: 8,
+      title: 'Product 8',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      price: 35,
+      ratings: 3,
+      reviews: 0,
+      isAddedToCart: false,
+      isAddedBtn: false,
+      isFavourite: false,
+      quantity: 1
+    },
+    {
+      id: 9,
+      title: 'Product 9',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+      price: 110,
+      ratings: 4,
+      reviews: 2,
+      isAddedToCart: false,
+      isAddedBtn: false,
+      isFavourite: false,
+      quantity: 1
+    }
+  ],
+    }
+  },
+
+  mounted () {
+    for (let i = 1; i <= 20; i++) {
+      this.quantityArray.push(i);
+    }
+
+    if (this.$props.product.quantity > 1) {
+      this.selected = this.$props.product.quantity;
+    }
+  },
+
+  computed: {
+    isUserLogged () {
+      return this.$store.getters.isUserLoggedIn;
+    }
+  },
+
+  methods: {
+    addToCart (id) {
+      let data = {
+        id: id,
+        status: true
+      }
+      this.$store.commit('addToCart', id);
+      this.$store.commit('setAddedBtn', data);
+    },
+    removeFromCart (id) {
+      let data = {
+        id: id,
+        status: false
+      }
+      this.$store.commit('removeFromCart', id);
+      this.$store.commit('setAddedBtn', data);
+    },
+    saveToFavorite (id) {
+      let isUserLogged = this.$store.state.userInfo.isLoggedIn;
+
+      if (isUserLogged) {
+        this.$store.commit('addToFavourite', id);
+      } else {
+        this.$store.commit('showLoginModal', true);
+      }
+    },
+    removeFromFavourite (id) {
+      this.$store.commit('removeFromFavourite', id);
+    },
+    onSelectQuantity (id) {
+      let data = {
+        id: id,
+        quantity: this.selected
+      }
+      this.$store.commit('quantity', data);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ .details {
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+
+    &:hover {
+      border: 1px solid #51bafc;
+    }
+ }
+ .button,
+ .select {
+   z-index: 2;
+ }
+ .select {
+   position: absolute;
+   right: 15px;
+   bottom: 35px;
+ }
+ .card-content {
+   padding: 0;
+ }
+ .buttons {
+   margin: 0;
+ }
+</style>
+
+
